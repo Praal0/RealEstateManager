c1e34ab417b8db66c80d791b7761e4dc5d74cfd4
Praal0 <guerrero.dorian1@gmail.com>
2021-08-19 10:58:57 +0200

    P9_Dorian_Guerrero_Add_Dimens
    Add : new dimens, new screen for add and activity main and Detail

--- app/src/main/java/com/openclassrooms/realestatemanager/ui/MainActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/MainActivity.kt
@@ -2,59 +2,73 @@ package com.openclassrooms.realestatemanager.ui
 
 import android.os.Bundle
 import android.view.Menu
-import androidx.appcompat.app.ActionBarDrawerToggle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.core.view.GravityCompat
-import androidx.drawerlayout.widget.DrawerLayout
-import com.openclassrooms.realestatemanager.R
 import com.openclassrooms.realestatemanager.databinding.ActivityMainBinding
+import com.openclassrooms.realestatemanager.ui.detail.DetailFragment
+import com.openclassrooms.realestatemanager.ui.master.MasterFragment
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
     private lateinit var toolbar : Toolbar
-    private lateinit var drawer : DrawerLayout
+    private var detailFragment: DetailFragment? = null
+    private var masterFragment: MasterFragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initialize()
-        initNavigationdrawer()
+        onClickFab()
+        configureAndShowMasterFragment()
+        configureAndShowDetailFragment()
+
         setSupportActionBar(toolbar)
     }
 
-    // Initialisation drawer
-    private fun initNavigationdrawer() {
-        val toggle = ActionBarDrawerToggle(
-            this, drawer, toolbar,
-            R.string.navigation_drawer_open, R.string.navigation_drawer_close)
-        drawer.addDrawerListener(toggle)
-        toggle.syncState()
+    private fun configureAndShowMasterFragment() {
+        // Get FragmentManager (Support) and Try to find existing instance of fragment in FrameLayout container
+        masterFragment = getSupportFragmentManager().findFragmentById(com.openclassrooms.realestatemanager.R.id.frame_layout_main) as MasterFragment?
+
+        //A - We only add DetailFragment in Tablet mode (If found frame_layout_detail)
+        if (masterFragment == null ) {
+            masterFragment = MasterFragment()
+            supportFragmentManager.beginTransaction()
+                .add(com.openclassrooms.realestatemanager.R.id.frame_layout_main, masterFragment!!)
+                .commit()
+        }
+    }
+
+    private fun configureAndShowDetailFragment() {
+        //Get FragmentManager (Support) and Try to find existing instance of fragment in FrameLayout container
+        detailFragment = supportFragmentManager.findFragmentById(com.openclassrooms.realestatemanager.R.id.frame_layout_detail) as DetailFragment?
+
+        if (detailFragment == null && findViewById<View?>(com.openclassrooms.realestatemanager.R.id.frame_layout_detail) != null) {
+            //Create new main fragment
+            detailFragment = DetailFragment()
+            //Add it to FrameLayout container
+            supportFragmentManager.beginTransaction()
+                .add(com.openclassrooms.realestatemanager.R.id.frame_layout_detail, detailFragment!!)
+                .commit()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        menuInflater.inflate(com.openclassrooms.realestatemanager.R.menu.activity_main_menu, menu)
         return super.onCreateOptionsMenu(menu)
 
     }
 
-
-    override fun onBackPressed() {
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START)
-        } else {
-            super.onBackPressed()
-            finish()
-        }
+    fun onClickFab() {
+        binding.fabBtn.setOnClickListener {  }
     }
 
     // Initialisation variable
     private fun initialize() {
         toolbar = binding.includedToolbar.simpleToolbar
-        drawer = binding.drawerLayout
     }
 
 }
---
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/add/AddActivity.kt
@@ -0,0 +1,12 @@
+package com.openclassrooms.realestatemanager.ui.add
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.openclassrooms.realestatemanager.R
+
+class AddActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add)
+    }
+}
