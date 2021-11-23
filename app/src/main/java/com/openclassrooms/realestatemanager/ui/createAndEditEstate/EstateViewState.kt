94e1a791bd280bcece37e5c090b3cf1f9652b12f
Praal0 <guerrero.dorian1@gmail.com>
2021-11-23 11:22:22 +0100

    VIew State

--- app/src/main/java/com/openclassrooms/realestatemanager/ui/createAndEditEstate/EstateViewState.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/createAndEditEstate/EstateViewState.kt
@@ -2,14 +2,9 @@ package com.openclassrooms.realestatemanager.ui.createAndEditEstate
 
 import android.net.Uri
 
-class EstateViewState {
-    val id : String = ""
-    val name : String = ""
-    val photos : List<PhotoViewState> = ArrayList()
+data class EstateViewState(val photoList: List<Uri>) {
+    val photos : List<Uri> = photoList
 
-    data class PhotoViewState(
-        val id: String,
-        val url: Uri
-    )
+
 }
 
--- app/src/main/java/com/openclassrooms/realestatemanager/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/detail/DetailActivity.kt
@@ -28,7 +28,6 @@ class DetailActivity : BaseActivity() {
 
     private lateinit var activityDetailBinding: ActivityDetailBinding
     private lateinit var detailFragment: DetailFragment
-    private val viewModel: EstateViewModel by viewModels()
     private lateinit var toolbar : Toolbar
     private  var estateId : Long  = 0
     private var estate: Estate? = null
